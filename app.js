3b684f175a9fc304906b94897921d1da8b141537
a.alves.de.almeida <a.alves.de.almeida@avanade.com>
2022-10-03 14:03:53 -0300

    add title to course card

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ function getCard(course) {
                 <img src="./images/${course.image}" alt="${course.title}" class="bd-placeholder-img card-img-top"
                     width="100%" height="225" role="img">
                 <div class="card-body">
+                    <h3>${course.title}</h3>
                     <p class="card-text">${course.description}</p>
                     <a href="${course.link}" target="_blank" class="btn btn-sm btn-outline-secondary">View</a>
                 </div>
